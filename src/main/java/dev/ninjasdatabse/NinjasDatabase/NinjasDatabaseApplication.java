907ae884bc63cc9a19499c5f3fea8de5ae9d3358
Evelyn Fernandes <evelyn.fernandes4@gmail.com>
2026-07-06 19:24:20 -0300

    first commit

---
+++ src/main/java/dev/ninjasdatabse/NinjasDatabase/NinjasDatabaseApplication.java
@@ -0,0 +1,13 @@
+package dev.ninjasdatabse.NinjasDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjasDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjasDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ninjasdatabse/NinjasDatabase/NinjasDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ninjasdatabse.NinjasDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjasDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
